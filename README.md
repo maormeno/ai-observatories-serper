704ccf18c0f3a0317f67be0aefcffb3ffee66875
Mateo <maormeno@uc.cl>
2022-11-09 16:17:08 -0300

    gitignore deleted env and added Link model without migration

--- README.md
+++ README.md
@@ -6,4 +6,4 @@
 
 `source env/bin/activate`
 
-`python -m pip install -r requirements.txt`
+`pip3 install -r requirements.txt`
--- project/app/models.py
+++ project/app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Link(models.Model):
+    source = models.CharField(max_length=2048)
+    title = models.CharField(max_length=2048)
+    country = models.CharField(max_length=256)
+    keyword1 = models.CharField(max_length=256)
+    keyword2 = models.CharField(max_length=256)
+    label = models.CharField(max_length=64)
+    
+    
